21391bf47ba58ab08a67b1b0ff646033a3acd303
NacaliLozano <nakaly@gmail.com>
2024-07-28 13:36:34 -0400

    Add find the dominant emotion functionality

--- emotion_detection.py
+++ emotion_detection.py
@@ -13,6 +13,25 @@ def emotion_detector(text_to_analyze):
 
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=myobj, headers=header)
+    
+    # Parse the response from the API
+    formatted_response = json.loads(response.text)
 
-    # Return the text
-    return response.text
+    # Find the dominant emotion
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    dominant_emotion = {"emotion": "", "score": 0.0}
+    for key, value in emotions.items():
+        if value > dominant_emotion["score"]:
+            dominant_emotion["emotion"] = key
+            dominant_emotion["score"] = value
+    
+    # Construct the result
+    result = {}
+    result["anger"] = emotions["anger"]
+    result["disgust"] = emotions["disgust"]
+    result["fear"] = emotions["fear"]
+    result["joy"] = emotions["joy"]
+    result["sadness"] = emotions["sadness"]
+    result["dominant_emotion"] = dominant_emotion["emotion"]
+    
+    return result

1a06a950fb2d0eed98c951f011a0640fc0f27009
NacaliLozano <nakaly@gmail.com>
2024-07-29 14:30:50 -0400

    Add error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -14,24 +14,34 @@ def emotion_detector(text_to_analyze):
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=myobj, headers=header)
     
-    # Parse the response from the API
-    formatted_response = json.loads(response.text)
-
-    # Find the dominant emotion
-    emotions = formatted_response["emotionPredictions"][0]["emotion"]
-    dominant_emotion = {"emotion": "", "score": 0.0}
-    for key, value in emotions.items():
-        if value > dominant_emotion["score"]:
-            dominant_emotion["emotion"] = key
-            dominant_emotion["score"] = value
-    
-    # Construct the result
     result = {}
-    result["anger"] = emotions["anger"]
-    result["disgust"] = emotions["disgust"]
-    result["fear"] = emotions["fear"]
-    result["joy"] = emotions["joy"]
-    result["sadness"] = emotions["sadness"]
-    result["dominant_emotion"] = dominant_emotion["emotion"]
+    # Check data validity
+    if response.status_code == 400:
+        # Construct the result
+        result["anger"] = None
+        result["disgust"] = None
+        result["fear"] = None
+        result["joy"] = None
+        result["sadness"] = None
+        result["dominant_emotion"] = None
+    elif response.status_code == 200:
+        # Parse the response from the API
+        formatted_response = json.loads(response.text)
+
+        # Find the dominant emotion
+        emotions = formatted_response["emotionPredictions"][0]["emotion"]
+        dominant_emotion = {"emotion": "", "score": 0.0}
+        for key, value in emotions.items():
+            if value > dominant_emotion["score"]:
+                dominant_emotion["emotion"] = key
+                dominant_emotion["score"] = value
+        
+        # Construct the result
+        result["anger"] = emotions["anger"]
+        result["disgust"] = emotions["disgust"]
+        result["fear"] = emotions["fear"]
+        result["joy"] = emotions["joy"]
+        result["sadness"] = emotions["sadness"]
+        result["dominant_emotion"] = dominant_emotion["emotion"]
     
     return result
